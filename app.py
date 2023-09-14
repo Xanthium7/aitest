45b43fabda58655b54408f34a1576bc790c7dfcf
Akshhay <=>
2023-09-14 22:03:14 +0530

    added the necessary codes, but the free tiral of gpt expired

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("hey")
+import os
+import openai
+
+openai.api_key = "API KEY"
+
+
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {
+      "role": "user",
+      "content": "define man in a line"
+    }
+  ]
+)
+print(response)
